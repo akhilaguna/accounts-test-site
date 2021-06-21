9144cfc9df140c47acf72d7472f683cfa9d1396a
akhilaguna <mraig1999@gmail.com>
2021-06-21 15:24:07 +0530

    added functions file

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -5,9 +5,31 @@ if(isset($_POST['submit']))
  $email = $_POST['email'];
  $username = $_POST['uid'];
  $pwd = $_POST['pwd'];
- $pwd = $_POST['pwdrepeat'];
+ $pwdrepeat = $_POST['pwdrepeat'];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if(emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat) !== false)
+    {
+        header("location: ../signup.php?erro=emptyinput");
+        exit();
+    }
+    if(invalidUid($username) !== false)
+    {
+        header("location: ../signup.php?erro=invaliduid");
+        exit();
+    }
+    if(invalidEmail($email) !== false)
+    {
+        header("location: ../signup.php?erro=invalidemail");
+        exit();
+    }
+    
+
 }
 else
 {
     header("location: ../signup.php");
+    exit();
 }
