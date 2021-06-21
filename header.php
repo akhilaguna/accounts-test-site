6b73c77c256a4576c8654b9cd5cd3f62423859b2
akhilaguna <mraig1999@gmail.com>
2021-06-21 14:09:39 +0530

    created footer and connected it

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='UTF-8'>
+  <title>Portfolio Homepage</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="nav-wrapper">
+      <div class="left-side">
+        <div class="nav-link-wrapper active-nav-link">
+          <a href="index.html">Home</a>
+        </div>
+
+        <div class="nav-link-wrapper">
+          <a href="about.html">About</a>
+        </div>
+      </div>
+
+      <div class="right-side">
+        <div class="brand">
+          JORDAN HUDGENS
+        </div>
+      </div>
+    </div>
+
+    <div class="content-wrapper">
